5ba0b626e47e63ac0383ac0e1fc1aa68c1ff849b
Chuger <i.chuger@gmail.com>
2011-09-23 15:57:44 +0300

    add nimbus skin, remove glowing

--- src/main/java/TimerApp.java
+++ src/main/java/TimerApp.java
@@ -44,6 +44,16 @@ public class TimerApp implements ActionListener {
 
     //entry point
     public static void main(String[] args) {
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // If Nimbus is not available, you can set the GUI to another look and feel.
+        }
         JFrame frame = new JFrame(APP_NAME);
         frame.setContentPane(new TimerApp().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
